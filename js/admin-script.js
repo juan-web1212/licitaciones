2afeaf61db8cd140580328da32631ce09b6bffe5
juan-web1212 <juan_kpo_14_37@hotmail.com>
2024-12-17 13:27:27 -0300

    Update admin-script.js

--- js/admin-script.js
+++ js/admin-script.js
@@ -1,35 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCUHHGPBdN2VAWaF_J7wYqZ54sPBxy1RFs",
-  authDomain: "tenderlicitaciones-9ba50.firebaseapp.com",
-  projectId: "tenderlicitaciones-9ba50",
-  storageBucket: "tenderlicitaciones-9ba50.firebasestorage.app",
-  messagingSenderId: "531013447459",
-  appId: "1:531013447459:web:07580f26176abe2fe62696",
-  measurementId: "G-E9RJCXF5MC"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-// Guardar usuarios en Firebase
-function guardarUsuario(user) {
-    const userRef = database.ref('usuarios');
-    userRef.push(user);
-}
-
-// Guardar licitadores en Firebase
-function guardarLicitador(licitador) {
-    const licitadorRef = database.ref('licitadores');
-    licitadorRef.push(licitador);
-};
 // Función para ordenar las columnas de una tabla
 function ordenarTabla(tablaID, columnaIndex) {
     const tabla = document.getElementById(tablaID);
