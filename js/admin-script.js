e92f3461c840a79166598c2b2c67919b9e70ca58
juan-web1212 <juan_kpo_14_37@hotmail.com>
2024-12-17 12:25:49 -0300

    Update admin-script.js

--- js/admin-script.js
+++ js/admin-script.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCUHHGPBdN2VAWaF_J7wYqZ54sPBxy1RFs",
   authDomain: "tenderlicitaciones-9ba50.firebaseapp.com",
