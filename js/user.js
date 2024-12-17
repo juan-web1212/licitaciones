93ff63d606cf88b11eb77018b3f32b35a17d23f2
juan-web1212 <juan_kpo_14_37@hotmail.com>
2024-12-17 18:31:12 -0300

    Update user.js

--- js/user.js
+++ js/user.js
@@ -124,23 +124,6 @@ function filtrarLicitadores() {
         }
     }
 }
-
-// Función para filtrar los productos
-function filtrarProductos() {
-    const filter = document.getElementById('productosFilter').value.toLowerCase();
-    const rows = document.getElementById('productosTable').getElementsByTagName('tr');
-    
-    for (let i = 1; i < rows.length; i++) {
-        let cells = rows[i].getElementsByTagName('td');
-        const producto = cells[0].textContent.toLowerCase();
-        if (producto.indexOf(filter) > -1) {
-            rows[i].style.display = '';
-        } else {
-            rows[i].style.display = 'none';
-        }
-    }
-}
-
 // Función para confirmar al salir
 function confirmarSalir() {
     const confirmation = confirm("¿Estás seguro de que quieres salir?");
